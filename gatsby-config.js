556849d5a893eb65e8eda0ca833a0d97a18c884f
Daniel <daniel.park@gatda.com>
2020-02-01 19:50:32 +0900

    md file blogging test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Daniel Blog`,
-    description: `Personal Development Blog`,
+    title: `Daniel's Blog`,
+    description: `Developer Blog`,
     author: `@ekklesia11`,
   },
   plugins: [
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown-pages`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
